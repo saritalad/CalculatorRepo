d5fbb03cf02d0553c1bac70c6a7b0f1a8a365054
saritalad <sarita.lad@rediffmail.com>
2024-01-15 12:51:55 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelBinding_In_MVC_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding_In_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string SubmitForm(IFormCollection form)
+        {
+            var UserName = form["username"].ToString();
+            var UserEmail = form["useremail"].ToString();
+           
+            return $"Username:{UserName} User Email :{UserEmail}";
+
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Submitform1(User user)
+        {
+            if(user!=null)
+            {
+                  if (ModelState.IsValid){
+
+                    return   $"Username:{user.UserName} User Email :{user.UserEmail}"; }
+
+            }
+
+            return "Some error Occured";
+
+
+
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ModelBinding_In_MVC_Core.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
